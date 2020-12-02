be763a063d3c1cd13178bbdd12c44e888d3002f8
GitHub Action <action@github.com>
2020-12-02 12:43:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/jonatha1983/dbtree/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jonatha1983/dbtree/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonatha1983.dbtree.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonatha1983.dbtree.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jonatha1983/dbtree/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jonatha1983/dbtree/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonatha1983.dbtree.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonatha1983.dbtree.MyBundle
 
 class MyProjectService(project: Project) {
 
